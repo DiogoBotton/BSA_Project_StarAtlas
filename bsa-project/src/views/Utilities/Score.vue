e15a84f60099a573da729224ad8540cbca5aa0d8
DiogoBotton <diogo.luo@gmail.com>
2022-04-18 11:51:03 -0300

    Add fetch para adquirir informacoes de score de naves, falta api liberar cors

--- bsa-project/src/views/Utilities/Score.vue
+++ bsa-project/src/views/Utilities/Score.vue
@@ -4,7 +4,21 @@
 
 <script>
 export default {
-
+  data(){
+    return{
+      score: null,
+    }
+  },
+  created(){
+    let url = 'https://app.brazilianstaratlas.com/?app=2&u=2&auth=00000000&task=getScoreRewards';
+    fetch(url, { method: 'GET'})
+    .then(resp => resp.json())
+    .then(x => {
+      this.score = x;
+      console.log(x)
+    })
+    .catch(erro => console.log(erro))
+  }
 }
 </script>
 
